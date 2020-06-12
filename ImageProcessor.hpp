94f445ef1d7a8bf06f4d045d828353bd2817d71c
aroradeep-DK <65984254+aroradeep-DK@users.noreply.github.com>
2020-06-12 14:38:14 +1200

    [FINAL] Tested
    Implementation:
    1) Download the ImageProcessor.hpp file and put it in the same folder as the robot.cpp file
    2) Include the file by writing: #include "ImageProcessor.hpp"
    Note: The include statement for the robot.hpp library in the robot.cpp file can be removed since it is already included in this class
    3) Initialize an instance of the ImageProcessor class by writing "ImageProcessor imageProcessor;" inside the while loop
    4) You should be able to access the values stored in the Result struct by writing "result.onLine" or "result.error" once the constructor has been called (step 3)
    Note: result.onLine a boolean which is set to true if the robot is on the line and false if not. result.error is a double which contains the error margin (negative meaning the line is on the right of the cameraView's center and positive meaning the line is on the left of the cameraView's center). If result.onLine is set to false (the robot can not detect a line), result.error is set to 0.

--- ImageProcessor.hpp
+++ ImageProcessor.hpp
@@ -34,8 +34,11 @@ ImageProcessor::ImageProcessor() {
  * stores it in an array
  */
 void ImageProcessor::getWhitePixles() {
+	// Check each pixel in row selected
 	for (int i = 0; i < cameraView.width; i++) {
+		// Get pixel whiteness
 		int pixelWhiteness = (int) get_pixel(cameraView, ROW, i, 3);
+		// Update array based on pixel colour
 		if (pixelWhiteness == 255) {
 			a[i] = 1;
 			sumOfWhiteIndexes += i;
@@ -51,14 +54,16 @@ void ImageProcessor::getWhitePixles() {
  * view's center and white line's center)
  */
 void ImageProcessor::calculateError() {
+	// Update array
 	getWhitePixles();
+	// Variables
 	double arrayCenter = cameraView.width/2.0;
 	double whiteLineCenter;
+	// Update result struct based on cameraView
 	if (numberOfWhitePixels > 0) {
 		whiteLineCenter = sumOfWhiteIndexes/numberOfWhitePixels;
 		result.onLine = true;
 		result.error = arrayCenter - whiteLineCenter;
-		
 	} else {
 		whiteLineCenter = arrayCenter;
 		result.onLine = false;
