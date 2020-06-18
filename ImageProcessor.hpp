039c5016dca35fdf14f02159afc7e5aafdf137ad
aroradeep-DK <65984254+aroradeep-DK@users.noreply.github.com>
2020-06-18 22:09:39 +1200

    Merged Core, Completion & Challenge
    Implementation:
    Implementation:
    1) Download the ImageProcessor.hpp file and put it in the same folder as the robot.cpp file
    2) Include the file by writing: #include "ImageProcessor.hpp"
    Note: The include statement for the robot.hpp library in the robot.cpp file can be removed since it is already included in this class
    3) Initialize an instance of the ImageProcessor class by writing "ImageProcessor imageProcessor;" inside the while loop
    
    This will give the robot.cpp class access to the Result struct containing the following values:
        - bool onLine ~ Whether or not the robot is on the line
        - bool foundWall ~ Whether or not the robot has found the wall of the maze (challenge)
        - double error ~ Distance from the cameraView's horizontal center to the center of the white line
        - double wallError ~ Distance from the cameraView's horizontal center to the right edge of the red wall
        - bool pathLeft ~ Whether or not the robot can go left
        - bool pathForward ~ Whether or not the robot can go forward
        - bool pathRight ~ Whether or not the robot can go right
    These can be accessed by attaching "result." before the variable name. For example, result.error or result.pathRight

--- ImageProcessor.hpp
+++ ImageProcessor.hpp
@@ -2,19 +2,32 @@
 
 // Result Struct
 struct Result {
+	bool onLine;
 	bool foundWall = false;
 	double error;
+	double wallError;
+	bool pathLeft;
+	bool pathForward;
+	bool pathRight;
 } result;
 
 class ImageProcessor {
 	private:
 		// Variables
-		int viewCenter;
-		int row;
-		int buffer = 20;
+		int sumOfPixelIndexes_F = 0;
+		int sumOfPixelIndexes_R = 0;
+		int sumOfPixelIndexes_L = 0;
+		int numberOfWhitePixels_F = 0;
+		int numberOfWhitePixels_R = 0;
+		int numberOfWhitePixels_L = 0;
+		int viewHorizontalCenter = (int) cameraView.width/2.0;
+		int viewVerticalCenter = (int) cameraView.height/2.0;
 		// Class Methods
+		void checkSides();
+		void checkPaths();
 		int distanceToWall();
 		void calculateError();
+		void updateResultStruct();
 	public:
 		// Constructor
 		ImageProcessor();
@@ -25,18 +38,48 @@ class ImageProcessor {
  */
 ImageProcessor::ImageProcessor() {
 	takePicture();
-	viewCenter = (int) (cameraView.width/2.0);
-	row = (int) (cameraView.height * 0.75);
-	calculateError();
+	updateResultStruct();
 }
 
 /**
+ * Checks if there are any pixels on each
+ * side of the cameraView (Left, Front & Right)
+ */
+void ImageProcessor::checkSides() {
+	// Scans the far front side for any white pixels
+	for (int i = 0; i < cameraView.width; i++) {
+		int pixelWhiteness_F = (int) get_pixel(cameraView, viewVerticalCenter - 2, i, 3);
+		if (pixelWhiteness_F > 250) {
+			sumOfPixelIndexes_F += i;
+			numberOfWhitePixels_F++;
+		}
+	}
+	// Scans the left and right sides for any white pixels
+	for (int i = 0; i < cameraView.height; i++) {
+		int pixelWhiteness_R = (int) get_pixel(cameraView, i, (int) (cameraView.width - 2), 3);
+		int pixelWhiteness_L = (int) get_pixel(cameraView, i, 2, 3);
+		if (pixelWhiteness_R > 250) {
+			sumOfPixelIndexes_R += i;
+			numberOfWhitePixels_R++;
+		}
+		if (pixelWhiteness_L > 250) {
+			sumOfPixelIndexes_L += i;
+			numberOfWhitePixels_L++;
+		}
+	}
+}
+
+
+/**
  * Calculates and returns the distance
- * from the cameraView's center to the
- * left wall's right edge.
+ * from the cameraView's horizontal center
+ * to the left wall's right edge.
  */
 int ImageProcessor::distanceToWall() {
-	for (int i = viewCenter; i > 0; i--) {
+	// Declare row to scan
+	int row = (int) (cameraView.height * 0.75);
+	// Scan row
+	for (int i = viewHorizontalCenter; i > 0; i--) {
 		int pixelRedness = (int) get_pixel(cameraView, row, i, 0);
 		int pixelBlueness = (int) get_pixel(cameraView, row, i , 2);
 		if ((pixelRedness > 250) && (pixelBlueness < 5)) {
@@ -48,25 +91,80 @@ int ImageProcessor::distanceToWall() {
 }
 
 /**
- * Calculates the error (difference between camera
- * view's center and white line's center)
+ * Calculates the distance from the cameraView's
+ * horizontal center to the center of the white
+ * line or right edge of the wall
  */
 void ImageProcessor::calculateError() {
+	// Get distance from view center to white line
+	checkSides();
+	double whiteLineCenter;
+	if (numberOfWhitePixels_F < 1) {
+		result.onLine = false;
+		result.error = 0;
+		numberOfWhitePixels_F = -1;
+	} else {
+		whiteLineCenter = sumOfPixelIndexes_F/numberOfWhitePixels_F;
+		result.onLine = true;
+		result.error = viewHorizontalCenter - whiteLineCenter;
+	}
+	// Error Checking (Prevent division by 0)
+	if (numberOfWhitePixels_L < 1) {
+		numberOfWhitePixels_L = -1;
+	}
+	if (numberOfWhitePixels_R < 1) {
+		numberOfWhitePixels_R = -1;
+	}
+
+	// Get distance from view center to red wall
 	int distanceFromCenter = distanceToWall();
 	// Check if maze has been detected
+	int buffer = 20;
 	if (result.foundWall) {
 		if (distanceFromCenter == -1) {
 			// Left
-			result.error = -100;
+			result.wallError = -100;
 		} else {
 			// Centralize robot
-			result.error = buffer - distanceFromCenter;
+			result.wallError = buffer - distanceFromCenter;
 		}
 	} else {
 		if (distanceFromCenter != -1) {
 			// Straight
 			result.foundWall = true;
-			result.error = buffer - distanceFromCenter;
+			result.wallError = buffer - distanceFromCenter;
 		}
 	}
+}
+
+void ImageProcessor::checkPaths() {
+	// Get the line's center for each side
+	int forwardLineCenter = sumOfPixelIndexes_F/numberOfWhitePixels_F;
+	int leftLineCenter = sumOfPixelIndexes_L/numberOfWhitePixels_L;
+	int rightLineCenter = sumOfPixelIndexes_R/numberOfWhitePixels_R;
+	// Check which paths are available to the robot
+	if ((forwardLineCenter > viewHorizontalCenter - 10) && (forwardLineCenter < viewHorizontalCenter + 10)) {
+		result.pathForward = true;
+	} else {
+		result.pathForward = false;
+	}
+	if ((leftLineCenter > viewVerticalCenter - 10) && (leftLineCenter < viewVerticalCenter + 10)) {
+		result.pathLeft = true;
+	} else {
+		result.pathLeft = false;
+	}
+	if ((rightLineCenter > viewVerticalCenter - 10) && (rightLineCenter < viewVerticalCenter + 10)) {
+		result.pathRight = true;
+	} else {
+		result.pathRight = false;
+	}
+}
+
+/** Updates the Result struct which
+ * holds all the information requirec
+ * by the robot to navigate the map
+ */
+void ImageProcessor::updateResultStruct() {
+	calculateError();
+	checkPaths();
 }
